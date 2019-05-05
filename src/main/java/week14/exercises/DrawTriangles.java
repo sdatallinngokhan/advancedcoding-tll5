63db98908e28a34856af9d8f41f64c2673f37efb
gokhanpolat <gokhanpolat203@aof.anadolu.edu.tr>
2019-05-05 10:00:06 +0300

    triangle examples

--- src/main/java/week14/exercises/DrawTriangles.java
+++ src/main/java/week14/exercises/DrawTriangles.java
@@ -18,7 +18,33 @@ public class DrawTriangles {
                 System.out.print(" ");
             }
 
-            for (int j =1; j<=i; j++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public void drawTriangle3(int count) {
+        System.out.println();
+        for (int i = count; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public void drawTriangle4(int count) {
+        System.out.println();
+
+        for (int i = count; i >= 1; i--) {
+            for (int j = count - i; j >= 1; j--) {
+                System.out.print(" ");
+            }
+
+            for (int j = i; j>= 1; j--) {
                 System.out.print("*");
             }
 
@@ -29,10 +55,12 @@ public class DrawTriangles {
     public static void main(String[] args) {
         DrawTriangles drawTriangles = new DrawTriangles();
 
-        int count = 7;
+        int count = 5;
 
         drawTriangles.drawTriangle1(count);
         drawTriangles.drawTriangle2(count);
+        drawTriangles.drawTriangle3(count);
+        drawTriangles.drawTriangle4(count);
     }
 
 }
